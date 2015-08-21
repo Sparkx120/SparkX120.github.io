2fdba6d2f6a7765956466e4a29280e899561a8d9
James <sparkx.os.120@gmail.com>
2015-08-21 01:51:58 -0400

    Ooooh Shiny

--- raytracer/2dray.js
+++ raytracer/2dray.js
@@ -15,20 +15,27 @@
 	// },100);
 	
 	var camera = new Camera({
-		position: {x:0, y:16, z:1, h:1},
-		gaze: {x:0, y:0, z:0, h:1},
-		width: canvas2D.width,
-		height: canvas2D.height,
+		position:     {x:0, y:16, z:2.5, h:1},
+		gaze:         {x:0, y:0,  z:0, h:1},
+		width:        canvas2D.width,
+		height:       canvas2D.height,
 		viewingAngle: 90,
-		world: null,
-		noPipe: false
+		world:        null,
+		noPipe:       false
 	});
 
-	var sphere = new Sphere({baseC: {r:0, g:0, b:255, a:255}}); //Create Generic Sphere
+	var sphere = new Sphere({baseC: {r:0, g:0, b:255, a:255},
+							 specularC: {r:255, g:255, b:255, a:255}
+							}); //Create Generic Sphere
+	var olight = new OmniLight({intensity:1.8,
+								source:{x:-8, y:12, z: 1, h:1}}); //Create an OmniLight
 
 	var world = [];
 	world.push(camera);
 	world.push(sphere);
+	world.push(olight);
+
+	console.log(world);
 
 
 	var raytracer = new Raytracer({
